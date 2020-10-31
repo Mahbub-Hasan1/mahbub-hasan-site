c3b1e6a5a5ad5e0c7ba274f0136611d4394fcef2
Md-Mahbub-Ahmed <67556397+Md-Mahbub-Ahmed@users.noreply.github.com>
2020-10-31 14:02:39 +0600

    done

--- src/Components/NoMatch/NoMatch.js
+++ src/Components/NoMatch/NoMatch.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NoMatch = () => {
     return (
-        <div>
+        <div className="container text-center">
+            <img src="https://cdn.pixabay.com/photo/2016/04/24/13/24/error-1349562__340.png"></img>
             <h2>This page not puond</h2>
+            <Link to="/">
+                <input type="submit" value="Back to Home"  style={{ width: '40%', backgroundColor: '#111430', marginLeft: '15px' }}  className="btn btn-primary btn-lg"/>
+            </Link>
         </div>
     );
 };
